2f42acb0983d2e5500faca270fcdea0c63371066
Dawid Damek <dawid.damek98@gmail.com>
2022-05-27 19:45:36 +0200

    refactor avatar choose

---
+++ app/components/avatar-chooser/component.js
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+
+export default class AvatarChooserComponent extends Component {
+  avatarNames = [
+    'lion',
+    'pig',
+    'bear',
+    'bunny',
+    'cat',
+    'cock',
+    'dragon',
+    'elephant',
+    'fox',
+    'giraffe',
+    'monkey',
+    'owl',
+    'panda',
+    'tiger',
+  ];
+}

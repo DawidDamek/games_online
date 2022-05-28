32352332c0e60480fe8d147bc473e1aca75bda48
Dawid Damek <dawid.damek98@gmail.com>
2022-05-28 23:11:20 +0200

    enable to change user profile data

--- app/home/user-settings/route.js
+++ app/home/user-settings/route.js
@@ -1,13 +1,12 @@
 import Route from '@ember/routing/route';
-import { storageFor } from 'ember-local-storage';
 import { inject as service } from '@ember/service';
 
 export default class SettingsRoute extends Route {
-  @service store;
-  @storageFor('logged-as') loggedAs;
+  @service session;
+
+  checker = true;
 
   async model() {
-    const user = await this.store.findRecord('user', this.loggedAs.get('id'));
-    return user;
+    return this.session.currentUser;
   }
 }

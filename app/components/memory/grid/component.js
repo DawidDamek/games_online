089617b09fbbe9611d2022bce6648ebc6b8ac624
Dawid Damek <dawid.damek98@gmail.com>
2022-06-15 21:57:48 +0200

    prevent from flipping matched card

--- app/components/memory/grid/component.js
+++ app/components/memory/grid/component.js
@@ -1,12 +1,13 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
-import { later, cancel } from '@ember/runloop';
+import { later } from '@ember/runloop';
 
 export default class MemoryGridComponent extends Component {
   @tracked cardsChosenNames = [];
   @tracked cardsChosenIds = [];
   @tracked cardsTemplateElement = [];
+  @tracked clickedShuffledCards = [];
   @tracked result = 0;
   @tracked flipCount = 0;
 
@@ -15,61 +16,73 @@ export default class MemoryGridComponent extends Component {
       name: 'fries',
       img: '/assets/images/fries.png',
       cardId: 1,
+      isMatched: false,
     },
     {
       name: 'cheeseburger',
       img: '/assets/images/cheeseburger.png',
       cardId: 2,
+      isMatched: false,
     },
     {
       name: 'hotdog',
       img: '/assets/images/hotdog.png',
       cardId: 3,
+      isMatched: false,
     },
     {
       name: 'icecream',
       img: '/assets/images/ice-cream.png',
       cardId: 4,
+      isMatched: false,
     },
     {
       name: 'milkshake',
       img: '/assets/images/milkshake.png',
       cardId: 5,
+      isMatched: false,
     },
     {
       name: 'pizza',
       img: '/assets/images/pizza.png',
       cardId: 6,
+      isMatched: false,
     },
     {
       name: 'fries',
       img: '/assets/images/fries.png',
       cardId: 7,
+      isMatched: false,
     },
     {
       name: 'cheeseburger',
       img: '/assets/images/cheeseburger.png',
       cardId: 8,
+      isMatched: false,
     },
     {
       name: 'hotdog',
       img: '/assets/images/hotdog.png',
       cardId: 9,
+      isMatched: false,
     },
     {
       name: 'icecream',
       img: '/assets/images/ice-cream.png',
       cardId: 10,
+      isMatched: false,
     },
     {
       name: 'milkshake',
       img: '/assets/images/milkshake.png',
       cardId: 11,
+      isMatched: false,
     },
     {
       name: 'pizza',
       img: '/assets/images/pizza.png',
       cardId: 12,
+      isMatched: false,
     },
   ];
 
@@ -77,15 +90,17 @@ export default class MemoryGridComponent extends Component {
 
   @action
   flipCard(event) {
-    if (this.flipCount === 2) {
-      return;
-    }
-
     const clickedCard = this.shuffledCards.find((card) => {
       return card.cardId === parseInt(event.target.getAttribute('data-id'));
     });
+
+    if (this.flipCount === 2 || clickedCard.isMatched) {
+      return;
+    }
+
     this.cardsChosenIds.push(event.target.getAttribute('data-id'));
     this.cardsTemplateElement.push(event.target);
+    this.clickedShuffledCards.unshift(clickedCard);
     event.target.setAttribute('src', [clickedCard.img]);
     this.cardsChosenNames.push(clickedCard.name);
     this.flipCount = this.flipCount + 1;
@@ -102,7 +117,6 @@ export default class MemoryGridComponent extends Component {
   }
 
   checkMatch(event) {
-    console.log('check match');
     const card = event.target;
     const optionOneId = this.cardsChosenIds[0];
     const optionTwoId = this.cardsChosenIds[1];
@@ -110,14 +124,12 @@ export default class MemoryGridComponent extends Component {
     if (optionOneId == optionTwoId) {
       alert('you have clicked the same image!');
       card.setAttribute('src', '/assets/images/blank.png');
-      console.log(this.flipCount);
     } else {
       if (this.cardsChosenNames[0] == this.cardsChosenNames[1]) {
-        console.log('para');
         this.result = this.result + 1;
-        console.log(this.flipCount);
+        this.clickedShuffledCards[0].isMatched = true;
+        this.clickedShuffledCards[1].isMatched = true;
       } else {
-        console.log('nie para');
         this.cardsTemplateElement[0].setAttribute(
           'src',
           '/assets/images/blank.png'
@@ -126,7 +138,6 @@ export default class MemoryGridComponent extends Component {
           'src',
           '/assets/images/blank.png'
         );
-        console.log(this.flipCount);
       }
       this.flipCount = 0;
       this.cardsChosenNames = [];

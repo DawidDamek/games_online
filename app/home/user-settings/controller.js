139b8853f0d234ada42d3e288939d537e5e5365e
Anna Pawlik <AnnaP@Safari.local>
2022-07-04 10:29:28 +0200

    fix tests

--- app/home/user-settings/controller.js
+++ app/home/user-settings/controller.js
@@ -1,7 +1,5 @@
 import Controller from '@ember/controller';
-import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
-import { Changeset } from 'ember-changeset';
 
 export default class UserSettingsController extends Controller {
   @service store;

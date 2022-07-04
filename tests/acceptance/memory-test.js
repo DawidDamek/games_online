179c4ad71c0fbc89265be7565ae734491521b24e
Anna Pawlik <AnnaP@Safari.local>
2022-07-04 18:48:29 +0200

    fix acceptance test

--- tests/acceptance/memory-test.js
+++ tests/acceptance/memory-test.js
@@ -1,12 +1,24 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, click } from '@ember/test-helpers';
+import { visit, currentURL, click, waitFor } from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 module('Acceptance | memory', function (hooks) {
   setupApplicationTest(hooks);
 
+  hooks.beforeEach(function () {
+    window.localStorage.setItem(
+      'storage:logged-as',
+      JSON.stringify({ id: '1' })
+    );
+  });
+
+  hooks.afterEach(function () {
+    window.localStorage.clear();
+  });
+
   test('visiting /memory', async function (assert) {
     await visit('/memory');
+    await waitFor('[data-test-start-button]');
     await click('[data-test-start-button]');
 
     assert.strictEqual(currentURL(), '/memory');

b16ccee9818b23f2b74b3088bd66a5d2d8562bf4
Dawid Damek <dawid.damek98@gmail.com>
2022-07-08 15:19:54 +0200

    add singleplayer mode to tic-tac-toe game

--- app/components/tic-tac-toe/game/component.js
+++ app/components/tic-tac-toe/game/component.js
@@ -1,7 +1,7 @@
 import Component from '@glimmer/component';
 import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
-import { cloneDeep } from 'lodash';
+import { cloneDeep, random } from 'lodash';
 
 export default class TicTacToeComponent extends Component {
   @tracked blocks = Array.from({ length: 9 }, (_, id) => ({
@@ -11,6 +11,8 @@ export default class TicTacToeComponent extends Component {
   @tracked nextMove = '𝗫';
   @tracked isStarted = false;
   @tracked winner = null;
+  @tracked isSingleplayer = true;
+  @tracked computerPossibleBlocks = [];
 
   matrix = [
     [0, 1, 2],
@@ -22,6 +24,17 @@ export default class TicTacToeComponent extends Component {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
+  get shouldBeDisabled() {
+    return Boolean(this.isStarted);
+  }
+
+  get gameMode() {
+    if (this.isSingleplayer) {
+      return 'Singleplayer';
+    }
+    return 'Multiplayer';
+  }
 
   get shouldBeAbleToClick() {
     return Boolean(!this.winner);
@@ -36,7 +49,27 @@ export default class TicTacToeComponent extends Component {
   }
 
   @action
+  onChangeGameMode() {
+    this.isSingleplayer = !this.isSingleplayer;
+  }
+
+  @action
   onClick(event) {
+    if (this.isSingleplayer) {
+      if (this.shouldBeAbleToClick) {
+        let newGrid = this.blocks;
+        let blockState = newGrid.find(
+          (block) => block.id === JSON.parse(event.target.id)
+        );
+        if (blockState.symbol !== null) {
+          return;
+        }
+        blockState.symbol = this.nextMove;
+        this.blocks = cloneDeep(newGrid);
+        this.computerClick();
+        this.checkWinner();
+      }
+    }
     if (this.shouldBeAbleToClick) {
       let newGrid = this.blocks;
       let blockState = newGrid.find(
@@ -89,6 +122,19 @@ export default class TicTacToeComponent extends Component {
     if (this.blocks.every((block) => block.symbol !== null)) {
       this.winner = 'Nobody';
     }
+  }
+
+  computerClick() {
+    if (this.blocks.every((block) => block.symbol !== null)) {
+      return;
+    }
+    this.computerPossibleBlocks = this.blocks.filter((block) => {
+      return block.symbol === null;
+    });
+    // console.log(this.computerPossibleBlocks);
+    this.computerPossibleBlocks[
+      random(0, this.computerPossibleBlocks.length - 1)
+    ].symbol = 'O';
   }
 
   willDestroy() {

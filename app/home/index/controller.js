3b3f7c6ca08d64ba9129dbdfc8fefc18dd7eb5db
Dawid Damek <dawid.damek98@gmail.com>
2022-07-18 17:37:49 +0200

    correct position of id attribute

--- app/home/index/controller.js
+++ app/home/index/controller.js
@@ -2,17 +2,14 @@ import Controller from '@ember/controller';
 export default class IndexController extends Controller {
   get topMemoryPlay() {
     return this.model
-      .filter((play) => {
-        return play.gameName === 'Memory';
-      })
+      .filter(({ gameName }) => gameName === 'Memory')
       .sortBy('points')
       .reverse().firstObject;
   }
+
   get topWhacAMolePlay() {
     return this.model
-      .filter((play) => {
-        return play.gameName === 'Whac a Mole';
-      })
+      .filter(({ gameName }) => gameName === 'Whac a Mole')
       .sortBy('points')
       .reverse().firstObject;
   }

e6b1935c1c20d140f326be9b982c0b177e57808d
Dawid Damek <dawid.damek98@gmail.com>
2022-07-19 14:56:06 +0200

    add tests

--- tests/integration/tic-tac-toe/game/component-test.js
+++ tests/integration/tic-tac-toe/game/component-test.js
@@ -2,7 +2,6 @@ import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
-import TicTacToeComponent from 'games-online/components/tic-tac-toe/game/component';
 
 module('integration | Component | tictactoe/game', function (hooks) {
   setupRenderingTest(hooks);
@@ -14,15 +13,6 @@ module('integration | Component | tictactoe/game', function (hooks) {
 
     this.set('sessionService.currentUser', user);
 
-    class EnrichedTicTacToeComponent extends TicTacToeComponent {
-      shuffleCards() {
-        this.shuffledCards = this.cardArray;
-      }
-    }
-    this.owner.register(
-      'component:tic-tac-toe/game',
-      EnrichedTicTacToeComponent
-    );
     await render(hbs`<TicTacToe::Game />`);
   });
 

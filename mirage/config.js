4110e6cd0d2b6b8b61392789a8a29d23dcfab9ef
Dawid Damek <dawid.damek98@gmail.com>
2022-07-25 09:32:33 +0200

    apply review changes

--- mirage/config.js
+++ mirage/config.js
@@ -1,22 +1,8 @@
 export default function () {
-  // These comments are here to help you get started. Feel free to delete them.
-  /*
-    Config (with defaults).
-
-    Note: these only affect routes defined *after* them!
-  */
   // this.urlPrefix = '';    // make this `http://localhost:8080`, for example, if your API is on a different server
   // this.namespace = '';    // make this `/api`, for example, if your API is namespaced
   // this.timing = 400;      // delay for each request, automatically set to 0 during testing
-  /*
-    Shorthand cheatsheet:
 
-    this.get('/posts');
-    this.post('/posts');
-    this.get('/posts/:id');
-    this.put('/posts/:id'); // or this.patch
-    this.del('/posts/:id');
-*/
   this.get('/users');
   this.get('/users/:id');
   this.post('/users');
@@ -26,7 +12,4 @@ export default function () {
   this.get('/gameHistories/:id');
   this.post('/gameHistories');
   this.put('/gameHistories/:id');
-  /*
-    https://www.ember-cli-mirage.com/docs/route-handlers/shorthands
-  */
 }

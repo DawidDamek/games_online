6d07cf9ae98d56d706860daefec0fa5bd91d0ea6
Dawid Damek <dawid.damek98@gmail.com>
2023-03-30 16:32:30 +0200

    fix auth0

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,6 +4,9 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function (defaults) {
   let app = new EmberApp(defaults, {
+    dotEnv: {
+      clientAllowedKeys: ['AUTH0_DOMAIN', 'AUTH0_CLIENT_ID'],
+    },
     sassOptions: {
       includePaths: ['node_modules/bootstrap/scss'],
     },
